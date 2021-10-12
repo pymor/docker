6fee093ea93f8483c8961bf3b931ca20d343dbb8
René Fritze <rene.fritze@wwu.de>
2021-10-12 13:06:35 +0200

    fixup devpi entrypoint

--- devpi/entrypoint.bash
+++ devpi/entrypoint.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
-
-devpi-server --restrict-modify root --start --host 127.0.0.1 --port 3141 --init
-devpi-server --status
+DEVPI_PASSWORD=fjre
+set -uxe
+devpi-init
+#((devpi-server --restrict-modify root --host 127.0.0.1 --port 3141) & jobs -p >/var/run/devpi.pid)
+devpi-server --restrict-modify root --host 127.0.0.1 --port 3141  & export DEVPI_PID=$!
+sleep 6s
 devpi use http://localhost:3141
 devpi login root --password=''
 devpi user -m root password="${DEVPI_PASSWORD}"
 devpi index -y -c public bases=root/pypi pypi_whitelist='*'
-devpi-server --stop
-devpi-server --status
+kill -15 ${DEVPI_PID}
 
 exec devpi-server --restrict-modify root --host 0.0.0.0 --port 3141
