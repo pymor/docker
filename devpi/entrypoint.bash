2a0efb74c7fdc8f96aa3b58ed92c8c18213e11f1
René Fritze <rene.fritze@wwu.de>
2021-10-12 18:28:51 +0200

    [devpi] increase startup grace period

--- devpi/entrypoint.bash
+++ devpi/entrypoint.bash
@@ -4,7 +4,7 @@ set -uxe
 devpi-init
 #((devpi-server --restrict-modify root --host 127.0.0.1 --port 3141) & jobs -p >/var/run/devpi.pid)
 devpi-server --restrict-modify root --host 127.0.0.1 --port 3141  & export DEVPI_PID=$!
-sleep 6s
+sleep 15s
 devpi use http://localhost:3141
 devpi login root --password=''
 devpi user -m root password="${DEVPI_PASSWORD}"
