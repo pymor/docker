60148c97a17dd84abfbdc1490785036c160463af
René Fritze <rene.fritze@wwu.de>
2021-10-26 16:47:56 +0200

    add a semaphore for the compose tests

--- .ci/template.gitlab.py
+++ .ci/template.gitlab.py
@@ -136,7 +136,8 @@ test compose {{mirror}} {{PY[0]}} {{PY[2]}}:
     stage: test
     extends: .base
     {{ never_on_schedule_rule() }}
-
+    resource_group: compose
+    needs: ["parameterized_targets {{PY[0]}} {{PY[2]}}"]
     script:
         - make pypi-mirror_test_{{PY}}
 
