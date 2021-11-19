4a222dd10655ae547632a11b9d45ba2f6ae9c2ba
Felix Schindler <felix.schindler@wwu.de>
2021-11-19 09:59:18 +0100

    [jupyter] allow root in entrypoint

--- jupyter/entrypoint.sh
+++ jupyter/entrypoint.sh
@@ -3,6 +3,20 @@
 USER_ID=${LOCAL_USER_ID:-1000}
 
 echo "Starting with UID : $USER_ID"
+
+# we are running as root (if USER_ID == 0) there is little we can do but continue ...
+if [[ $UID_ == 0 ]] ; then
+  mkdir -p /home/pymor
+  export HOME=/home/pymor
+  if [ "X$@" == "X" ]; then
+    exec /bin/bash
+  else
+    exec "$@"
+  fi
+  exit
+fi
+
+# else create user pymor with correct id
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m pymor && \
     adduser pymor sudo &&\
     echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
