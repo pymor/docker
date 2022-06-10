93eae44e6a55f262e5fee00b6e265f652095c79f
René Fritze <rene.fritze@wwu.de>
2022-06-10 16:55:00 +0200

    exclude scripts in bin dir from import check

--- pypi-mirror_test/check_import.py
+++ pypi-mirror_test/check_import.py
@@ -4,4 +4,8 @@ import pkgutil
 from importlib import import_module
 
 for f in pkgutil.iter_modules():
+    print(f"importing {f}")
+    print(f"importing {f[0]}")
+    if f[0].path.startswith("/usr/local/bin"):
+        continue
     import_module(f[1])
