033c4c7d82a06066fa63903403a143285d78db0a
René Fritze <rene.fritze@wwu.de>
2022-08-22 10:56:07 +0200

    adds cppyy

--- ci_wheels/build_wheels.sh
+++ ci_wheels/build_wheels.sh
@@ -5,7 +5,7 @@ set -exu
 rm -rf /src
 mkdir /src
 pip install -U wheel pymor-oldest-supported-numpy~=2021.1.0 scikit-build
-declare -A VERSIONS=( ["slycot"]="~=0.4.0" ["mpi4py"]=">=3" ["gmsh"]="~=4.8.0" ["petsc4py"]="==${PETSC4PY_VERSION}" ["mpi4py"]=">=3")
+declare -A VERSIONS=( ["slycot"]="~=0.5.0" ["mpi4py"]=">=3" ["gmsh"]="~=4.8.0" ["petsc4py"]="==${PETSC4PY_VERSION}" ["mpi4py"]=">=3" ["cppyy-backend"]="==1.14.9" ["cppyy"]="==2.4.0")
 
 for pkg in ${WHEEL_PKGS} ; do
   cd /src
@@ -16,6 +16,8 @@ for pkg in ${WHEEL_PKGS} ; do
   pip wheel --use-feature=in-tree-build --no-build-isolation . -w ${WHEELHOUSE}/tmp
   mv ${WHEELHOUSE}/tmp/${pkg}* ${WHEELHOUSE}/
   rm -rf ${WHEELHOUSE}/tmp
+  cd -
+  rm -rf ${pkg}*
 done
 
 pip download --no-deps torch==1.8.1+cpu torchvision==0.9.1+cpu torchaudio==0.8.1 \
